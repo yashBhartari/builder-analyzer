a343e3139d09eeb046f6eccb40cf4ce267f9f596
yashBhartari <ybhartari3@gmail.com>
2026-04-15 00:25:25 +0530

    fix: explicit SPA routing and Vercel function logging

--- api/index.js
+++ api/index.js
@@ -1,2 +1,8 @@
 import app from '../server/src/index.js';
-export default app;
+
+export default (req, res) => {
+  console.log('--- Vercel Function Hit ---');
+  console.log('Method:', req.method);
+  console.log('Path:', req.url);
+  return app(req, res);
+};
